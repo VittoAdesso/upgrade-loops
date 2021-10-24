80ebf71e403e19bc071354de97ca3684bb540ae9
Vittorio Adesso <adessovittorio8@gmail.com>
2021-10-24 19:41:49 +0200

    Iteración #3

---
+++ probandoForOf.js
@@ -0,0 +1,13 @@
+/* 1. Usa un bucle forof para recorrer todos los destinos del array. 
+2. Imprime en un console.log sus valores. */
+
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar'];
+
+/* Básicamente aquí lo que hacermos en declarar el bucle y dentro le Asignamos un let o const junto con of el array u objeto == es decir que le daremos valor a una nueva variable recorrdiendo
+el array y luego que me devuelva el resultado */
+for (const recorrido of placesToTravel) {
+
+    console.log(recorrido);
+
+}
